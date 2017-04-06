8e2be2a63288ecf903ca1345eb960a9554588bb3
llt <646773422@qq.com>
2017-04-06 15:52:54 +0800

    init third week's homework

---
+++ thirdWeek/Point.java
@@ -0,0 +1,18 @@
+public class Point implements Comparable<Point> {
+    private int m_x, int m_y;
+    public Point(int x, int y)                         // constructs the point (x, y)
+    {
+        m_x = x;
+        m_y = y;
+    }
+    public   void draw()                               // draws this point
+    public   void drawTo(Point that)                   // draws the line segment from this point to that point
+    public String toString()                           // string representation
+
+    public int compareTo(Point that)                   // compare two points by y-coordinates, breaking ties by x-coordinates
+    {
+        
+    }
+    public double slopeTo(Point that)                  // the slope between this point and that point
+    public Comparator<Point> slopeOrder()              // compare two points by slopes they make with this point
+}
