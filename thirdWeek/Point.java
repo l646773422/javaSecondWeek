27ce54e60ee7a694685e3f5a06f49e6f2677bf7b
llt <646773422@qq.com>
2017-04-07 10:26:24 +0800

    finish Point class, need debug

--- thirdWeek/Point.java
+++ thirdWeek/Point.java
@@ -1,8 +1,18 @@
 import edu.princeton.cs.algs4.StdDraw;
-
+import java.util.Comparator;
+import java.util.Arrays;
 public class Point implements Comparable<Point> 
 {
     private int m_x, m_y;
+    private Point m_point;
+    private class slopeComparator implements Comparator<Point>
+    {
+        public int compare( Point p1, Point p2)
+        {
+            // m_point some question
+            return (int)( m_point.slopeTo(p1) - m_point.slopeTo(p2) );
+        }
+    }
     
     private static int Clip3 (int value, int min, int max)
     {
@@ -13,6 +23,7 @@ public class Point implements Comparable<Point>
     {
         m_x = Clip3( x, 0, 32767);
         m_y = Clip3( y, 0, 32767);
+        m_point = this;
     }
     public   void draw()                               // draws this point
     {
@@ -21,6 +32,7 @@ public class Point implements Comparable<Point>
     }
     public   void drawTo(Point that)                   // draws the line segment from this point to that point
     {
+        StdDraw.setPenRadius();
         StdDraw.line( m_x, m_y, that.m_x, that.m_y );
     }
     public String toString()                           // string representation
@@ -38,26 +50,52 @@ public class Point implements Comparable<Point>
     }
     public double slopeTo(Point that)                  // the slope between this point and that point
     {
-        return 0;
+        if ( m_y == that.m_y) return 32767;
+        return (double)(m_y - that.m_y) / (double)(m_x - that.m_x);
     }
-//    public Comparator<Point> slopeOrder()              // compare two points by slopes they make with this point
-//    {
-//        
-//    }
+    public Comparator<Point> slopeOrder()              // compare two points by slopes they make with this point
+    {
+        return new slopeComparator();
+    }
+    
     public static void main(String[] args)
     {
-        Point p1, p2;
-        p1 = new Point(100, 100);
-        p2 = new Point(15000, 15000);
+        Point[] points = new Point[4];
+        Point[] temp = new Point[3];
+        points[0] = new Point(100, 100);
+        points[1] = new Point(15000, 1000);
+        points[2] = new Point(7100, 8000);
+        points[3] = new Point(12000, 13000);
         
+        temp[0] = new Point(15000, 1000);
+        temp[1] = new Point(7100, 8000);
+        temp[2] = new Point(12000, 13000);
+//        for(int i=0;i<3;i++)
+//        {
+//            temp[i] = points[i+1];
+//        }
+        
+        Arrays.sort( temp, points[0].slopeOrder() );
+        
+        System.out.println();
+        for(int i=0;i<4;i++)
+        System.out.println(points[i].m_point);
+        System.out.println(points[0]);
+        System.out.println();
+        
+        for( int k = 0; k < temp.length; k++ )
+        {
+            System.out.println( temp[k] );
+        }
+        System.out.println();
+        for( int k = 0; k < points.length; k++ )
+        {
+            System.out.println( points[k] );
+        }
         StdDraw.setPenColor(StdDraw.BLACK);
         StdDraw.enableDoubleBuffering();
         StdDraw.setXscale(0, 32768);
         StdDraw.setYscale(0, 32768);
-        p1.draw();
-        System.out.println(p1);
-        p2.draw();
-        p1.drawTo(p2);
         StdDraw.show();
     }
 }
